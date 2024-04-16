ebbeb7e4502217c67d04ad842e09b5a4f93c603f
LE VAN_ANH <vananhle1908@gmail.com>
2024-04-16 16:06:37 +0200

    entry page 90%

--- client/src/pages/EntryPage/EntryPage.jsx
+++ client/src/pages/EntryPage/EntryPage.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; 
-import './Entrypage.css'; 
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Entrypage.css";
+import background from "../../assets/images/background-entrypage.png";
+import logo from "../../assets/images/brainbox-logo.png";
 
 function EntryPage() {
-
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const navigateToHome = () => {
-    navigate('/entry-page'); 
- 
+    navigate("/entry-page"); 
   };
 
   return (
-    <div className="entry-page">
-      <img src="client/src/assets/images/background-entrypage.png" className="background" />
-      <img src="client/src/assets/images/brainbox-logo.png" className="logo" />
+    <div
+      className="entry-page"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <link
+        href="https://fonts.googleapis.com/css2?family=Itim&display=swap"
+        rel="stylesheet"
+      />
+      <p className="itim-style">Enter the world of Brain Box</p>
+      <img src={logo} className="logo" alt="Brain Box Logo" />
       <button onClick={navigateToHome} className="start-button">
-        Start Quiz
+        Let's Go
       </button>
     </div>
   );
 }
 
 export default EntryPage;
-
