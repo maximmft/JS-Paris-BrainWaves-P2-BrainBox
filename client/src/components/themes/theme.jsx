338b8fc9a8b875f1dc682b06d56db639b680dd06
maximmft <mmaufront@gmail.com>
2024-04-29 17:07:51 +0200

    scroll restauration

--- client/src/components/themes/theme.jsx
+++ client/src/components/themes/theme.jsx
@@ -4,14 +4,66 @@ import "./theme.css";
 import ThemeCard from "./themeCard";
 
 function Theme({ name, id }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/numberquestions/${id}`);
+    window.scrollTo(0, 0); // Scroll to the top of the window
+  };
+
   return (
     <div
-      className={`theme-card ${name === "Entertainment: Video Games" ? "video-games" : ""} ${name === "Entertainment: Japanese Anime & Manga" ? "manga" : ""} ${name === "Entertainment: Film" ? "film" : ""} ${name === "Animals" ? "animals" : ""} ${name === "Science & Nature" ? "nature" : ""} ${name === "History" ? "history" : ""} ${name === "Art" ? "art" : ""} ${name === "Entertainment: Music" ? "music" : ""} ${name === "Mythology" ? "mythology" : ""} ${name === "Geography" ? "geography" : ""} ${name === "Sports" ? "sports" : ""}${name === "General Knowledge" ? "all" : ""}${name === "Entertainment: Books" ? "books" : ""}${name === "Entertainment: Television" ? "television" : ""}${name === "Entertainment: Board Games" ? "board-games" : ""}${name === "Celebrities" ? "celebrities" : ""}${name === "Vehicles" ? "vehicles" : ""}${name === "Entertainment: Comics" ? "comics" : ""}`}
-      onClick={() => navigate(`/numberquestions/${id}`)} onKeyDown={() =>navigate(`/numberquestions/${id}`)} role="presentation">
+      className={`theme-card ${getClassName(name)}`}
+      onClick={handleClick}
+      onKeyDown={handleClick}
+      role="presentation"
+    >
       <ThemeCard name={name} id={id} />
     </div>
   );
+}
+
+function getClassName(name) {
+  switch (name) {
+    case "Entertainment: Video Games":
+      return "video-games";
+    case "Entertainment: Japanese Anime & Manga":
+      return "manga";
+    case "Entertainment: Film":
+      return "film";
+    case "Animals":
+      return "animals";
+    case "Science & Nature":
+      return "nature";
+    case "History":
+      return "history";
+    case "Art":
+      return "art";
+    case "Entertainment: Music":
+      return "music";
+    case "Mythology":
+      return "mythology";
+    case "Geography":
+      return "geography";
+    case "Sports":
+      return "sports";
+    case "General Knowledge":
+      return "all";
+    case "Entertainment: Books":
+      return "books";
+    case "Entertainment: Television":
+      return "television";
+    case "Entertainment: Board Games":
+      return "board-games";
+    case "Celebrities":
+      return "celebrities";
+    case "Vehicles":
+      return "vehicles";
+    case "Entertainment: Comics":
+      return "comics";
+    default:
+      return "";
+  }
 }
 
 Theme.propTypes = {
